02cdd31cfd67f230540ac5c4189dbcf221dd3671
RyanEbanks <ryankebanks@gmail.com>
2023-03-29 20:19:10 -0400

    Finished structuring the model

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,23 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -5,12 +5,35 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+ //Figure out if I need to do an on delete or onupdate
+ //RESTRICT, CASCADE, NO ACTION, SET DEFAULT or SET NULL
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  //Same thing here
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    //The product_id in product_tag seed isn't unique
+    unique: false
+  },
+  //Creating an alias for when the data is retrieved
+  as: 'product_tag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_id'
+});
 
 module.exports = {
   Product,
